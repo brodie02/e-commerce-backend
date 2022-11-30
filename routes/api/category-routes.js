936b08af304293d503c452278a3cb43450ae797c
brodie02 <marshallbrodie0@gmail.com>
2022-11-30 18:49:45 +1030

    Add put and delete for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,12 +47,49 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+      const categoryData = await Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  );
+
+  if (!categoryData) {
+    res.status(404).json({ message: 'No category found with that id!' });
+    return;
+  }
+
+  res.status(200).json(categoryData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
